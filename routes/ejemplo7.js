72bb75545ab2d01256472aa33b938656d2a08a5a
minatoth <thaniatkm@hotmail.com>
2015-10-31 09:41:03 -0400

    segundo cambio

---
+++ routes/ejemplo7.js
@@ -0,0 +1,7 @@
+console.log("typeof /=>" +typeof 7);
+console.log("typeof \"hola\"=>" +typeof "hola");
+console.log("typeof true=>" +typeof true);
+console.log("typeof undefined=>" +typeof undefined);
+console.log("typeof null=>" +typeof null);
+console.log("typeof new Date()=>" +typeof new Date());
+console.log("typeof new Function=>" +typeof new Function());
