23019194699a01d574014669c8655b57efb2aaa3
minatoth <thaniatkm@hotmail.com>
2015-10-31 09:36:26 -0400

    primera modificacion

---
+++ routes/ejemplo4.js
@@ -0,0 +1,15 @@
+//OPERACIONES Y EXPRESIONES
+console.log("SUMA DE NUMEROS 13+7:");
+console.log(13+7);
+console.log("RESTA DE NUMEROS 13-7:");
+console.log(13-7);
+console.log("EXPRESIONES CON PARENTESIS(8*2 -4)/3");
+console.log((8*2 -4)/3);
+console.log(13+7);
+console.log("13"+"7");
+console.log("13"+7);
+console.log(+"13"+7);
+//console.log("EJECUCION SE INTERRUMPE:"");//ERROR_DE_EJECUCION
+//console.log(8/*3);//ERROR_DE_EJECUCION
+//console.log("EJECUCION SE INTERRUMPE:"");//ERROR_DE_EJECUCION
+//console.log(8 3);
---
+++ routes/ejemplo5.js
@@ -0,0 +1,7 @@
+//OPERACIONES Y EXPRESIONES
+console.log("SUMA DE NUMEROS 13+7:");
+console.log(13+7);
+console.log("SIGNO DE UN NUMERO +13");
+console.log(+13);
+console.log("CONCATENACION DE STRINGS \"HOLA\"+\"PEPE\":");
+console.log("HOLA"+"PEPE");
